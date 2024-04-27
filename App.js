7224741be175f5950cf3ab87c414ccd52dff66f5
louaialfarra <156582409+louaialfarra@users.noreply.github.com>
2024-04-27 15:20:40 +0300

    added icone and search button in header
    desc

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import Toast from "react-native-toast-message";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -15,7 +16,7 @@ import OrderSuccess from "./screens/orderScreen";
 
 import Category from "./screens/category";
 import SearchScreen from "./screens/searchScreen";
-
+import { Feather } from "@expo/vector-icons";
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
@@ -23,7 +24,23 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={{
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "black", height: 120 },
+            headerTitleStyle: { fontWeight: "bold" },
+            //headerTitle: "Hooboo" for text
+            headerTitle: () => (
+              <View style={{ alignItems: "center" }}>
+                <Image
+                  source={require("./assets/logo/hooboo_logo.png")}
+                  style={{ height: 50, resizeMode: "contain" }}
+                />
+              </View>
+            ),
+            headerRight: () => <SearchButton />,
+          }}
+        >
           <Tab.Screen name="Searchname" component={SearchStackScreen} />
           <Tab.Screen name="Product List" component={ProductListStackScreen} />
           <Tab.Screen name="Cart" component={CartScreen} />
@@ -69,5 +86,26 @@ function CategoryListStack() {
       </Stack.Screen>
       <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
     </Stack.Navigator>
+  );
+}
+function SearchButton() {
+  const navigation = useNavigation();
+
+  const handleSearch = () => {
+    navigation.navigate("Search");
+  };
+
+  return (
+    <TouchableOpacity
+      style={{
+        height: 40,
+        width: 30,
+        marginRight: 20,
+        marginTop: 30,
+      }}
+      onPress={handleSearch}
+    >
+      <Feather name="search" size={28} color="white" />
+    </TouchableOpacity>
   );
 }
